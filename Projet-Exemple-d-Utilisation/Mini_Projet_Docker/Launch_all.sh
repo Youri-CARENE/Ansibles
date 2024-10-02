b187c6c249f76cd5388940817c5661ea7592e5d7
Yo17 <youri.carene@gmail.com>
2024-10-03 00:09:55 +0200

    Grand netoyage c'est meixu de garder un projet en main

--- Projet-Exemple-d-Utilisation/Mini_Projet_Docker/Launch_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the Ansible image
-docker-compose build
-
-# Run the Docker containers and start the Ansible playbook
-docker-compose up -d
-docker exec -it ansible ansible-playbook create_users.yml
